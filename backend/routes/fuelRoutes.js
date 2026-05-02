a0a15cfe8ef44bed0dde60b69e720d7dbe7b0043
sadaruwanoshan017-bit <sadaruwanoshan017@gmail.com>
2026-05-02 13:44:55 +0530

    Update fuelRoutes.js

--- backend/routes/fuelRoutes.js
+++ backend/routes/fuelRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { logFuel, getFuelLogs, updateFuelLog, deleteFuelLog } = require('../controllers/fuelController');
+const { logFuel, getFuelLogs, updateFuelLog, deleteFuelLog, downloadFuelReport } = require('../controllers/fuelController');
 const { protect } = require('../middleware/authMiddleware');
 const { uploadFuelReceipt } = require('../middleware/uploadMiddleware');
 
@@ -8,6 +8,7 @@ router.use(protect);
 
 router.post('/', uploadFuelReceipt.single('receiptPhoto'), logFuel);
 router.get('/:bookingId', getFuelLogs);
+router.get('/:bookingId/report', downloadFuelReport);
 router.put('/:id', updateFuelLog);
 router.delete('/:id', deleteFuelLog);
 
